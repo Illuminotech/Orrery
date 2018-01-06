6f7ced495351aebd0b34e2c5f1e2935a06f426a4
michael <michaelfgolden@gmail.com>
2018-01-05 16:20:23 -0800

    Initial Commit

---
+++ Orrery/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Orrery
+//
+//  Created by Michael Golden on 1/5/18.
+//  Copyright © 2018 norobostudios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
